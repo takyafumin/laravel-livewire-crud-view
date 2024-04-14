2a75189f5467ed46522f21412349452ca6b37796
takyafumin <takyafumin@gmail.com>
2024-04-14 18:46:05 +0900

    tailwindcss導入

--- crud-view/resources/views/livewire/crud-diagram.blade.php
+++ crud-view/resources/views/livewire/crud-diagram.blade.php
@@ -1,8 +1,29 @@
 <div>
-    <h1>CRUD Diagram</h1>
-    <ul>
-        @foreach($entities as $entity)
-            <li>{{ $entity['name'] }} - {{ $entity['description'] }}</li>
-        @endforeach
-    </ul>
+    <h1 class="text-2xl font-bold mb-4">CRUD Diagram</h1>
+    <div class="overflow-x-auto">
+        <table class="min-w-full divide-y divide-gray-200">
+            <thead class="bg-gray-50">
+                <tr>
+                    <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                        Name
+                    </th>
+                    <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                        Description
+                    </th>
+                </tr>
+            </thead>
+            <tbody class="bg-white divide-y divide-gray-200">
+                @foreach($entities as $entity)
+                <tr>
+                    <td class="px-6 py-4 whitespace-nowrap">
+                        <div class="text-sm font-medium text-gray-900">{{ $entity['name'] }}</div>
+                    </td>
+                    <td class="px-6 py-4 whitespace-nowrap">
+                        <div class="text-sm text-gray-900">{{ $entity['description'] }}</div>
+                    </td>
+                </tr>
+                @endforeach
+            </tbody>
+        </table>
+    </div>
 </div>
